9aea2220ca1e5ed821c1b698739c7c9c5dfdc96d
Johan Kanflo <johan.kanflo@bitfuse.net>
2015-09-28 14:06:06 +0200

    Added proxclient Proxclient tracks the nearest aircraft and also fetches its image

---
+++ google.py
@@ -0,0 +1,110 @@
+# Copyright (c) 2015 Johan Kanflo (github.com/kanflo)
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import urllib2
+import cStringIO
+import json
+import os
+import socket
+import logging
+from StringIO import StringIO
+import gzip
+
+log = logging.getLogger(__name__)
+
+# Perform a Goolge Image search for 'searchTerm' and return an one with
+# width minWidth and aspect ratio between 1.3 and 1.55
+# Returns (urlString, imageData) or (None, None) in case of en error
+def imageSearch(searchTerm, minWidth = 0):
+    searchTerm = searchTerm.replace(" ", "+")
+
+    headers = [ ('Accept-Language', 'en-us'),
+				('Accept-Encoding', 'gzip, deflate'),
+				('Connection', 'close'),
+				('Accept', 'application/json, text/javascript, */*; q=0.01'),
+				('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/600.8.9 (KHTML, like Gecko) Version/7.1.8 Safari/537.85.17'),
+				('DNT', '1')
+				 ]
+
+    proxy = urllib2.ProxyHandler({'http': '127.0.0.1:8888'})
+    fetcher = urllib2.build_opener(proxy)
+    fetcher.addheaders = headers
+
+    log.debug(">>> Googeling '%s'" % searchTerm)
+    startIndex = 0
+
+    while startIndex < 25:
+        searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + searchTerm + "&start=%d" % (startIndex)
+#        print searchUrl
+        try:
+            f = fetcher.open(searchUrl, timeout = 10)
+        except UnicodeEncodeError, e:
+            log.critical("Google UnicodeEncode error: %s" % (e))
+            log.critical("searchTerm : '%s'" % (searchTerm))
+            return (None, None)
+
+        data = f.read()
+        if f.info().get('Content-Encoding') == 'gzip':
+            buf = StringIO(data)
+            f = gzip.GzipFile(fileobj=buf)
+            data = f.read()
+
+        j = json.loads(data)
+#        print j
+        count = len(j['responseData']['results'])
+        startIndex += count
+        for i in range(0, count):
+    #        print j['responseData']['results'][i]
+            url = j['responseData']['results'][i]['unescapedUrl']
+            height = float(j['responseData']['results'][i]['height'])
+            width = float(j['responseData']['results'][i]['width'])
+            aspectRatio = width / height
+            if width >= minWidth and aspectRatio > 1.3 and aspectRatio < 1.55:
+                fileName, fileExtension = os.path.splitext(url)
+                if fileExtension != ".svg" and fileExtension != ".gif":
+                    log.debug("%d x %d (%.2f) - %s" % (width, height, aspectRatio, url))
+#                    break
+                    log.debug("Fetching %s" % url)
+                    opener = urllib2.build_opener()
+                    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+                    counter = 3
+                    while counter > 0:
+                        try:
+                            data = cStringIO.StringIO(opener.open(url, timeout = 10).read())
+                            return (url, data)
+                        except socket.timeout, e:
+                            # For Python 2.7
+                            log.critical("  Timeout, retrying")
+                            counter -= 1
+                            pass
+                        except urllib2.HTTPError, e:
+                            log.critical("HTTP error: %s" % (e))
+                            log.critical("searchTerm : '%s'" % (searchTerm))
+                            return (None, None)
+                        except UnicodeEncodeError, e:
+                            log.critical("UnicodeEncodeError error: %s" % (e))
+                            log.critical("searchTerm : '%s'" % (searchTerm))
+                            return (None, None)
+                        except urllib2.URLError, e:
+                            log.critical("URLError error: %s" % (e))
+                            log.critical("searchTerm : '%s'" % (searchTerm))
+                            return (None, None)
+    return (None, None)
---
+++ proxclient.py
@@ -0,0 +1,346 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2015 Johan Kanflo (github.com/kanflo)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#
+# proxclient subscribes to the MQTT topic /adsb/radar/json and calculates the
+# distance to the nearest aircraft and publishes data to /adsb/proximity/json
+#
+
+import paho.mqtt.client as mosquitto
+import Queue
+import time
+import argparse
+from threading import *
+import logging
+import sys
+try:
+  import remotelogger
+except ImportError:
+  print "remotelogger module not found, install from github.com/kanflo/python-remotelogger"
+  sys.exit(1)
+import socket
+import calendar, datetime
+import json
+import traceback
+import math
+import signal
+import random
+import google
+import planeimg
+
+max_receiver_distance = 374000 # Meters
+min_receiver_distance = 10000 # Meters
+
+# Used for tracking the nearest aircraft
+minTrackingDistance = 999999
+trackingICAO24 = None
+
+gQuitting = False
+gImageDB = None
+gCurImage = None
+
+log = logging.getLogger(__name__)
+
+
+def deg2rad(deg):
+  return deg * (math.pi/180)
+
+def receiverBearing(lat, lon):
+  global args
+  lat1, lon1 = args.lat, args.lon
+  lat2, lon2 = lat, lon
+ 
+  rlat1 = math.radians(lat1)
+  rlat2 = math.radians(lat2)
+  rlon1 = math.radians(lon1)
+  rlon2 = math.radians(lon2)
+  dlon = math.radians(lon2-lon1)
+ 
+  b = math.atan2(math.sin(dlon)*math.cos(rlat2),math.cos(rlat1)*math.sin(rlat2)-math.sin(rlat1)*math.cos(rlat2)*math.cos(dlon)) # bearing calc
+  bd = math.degrees(b)
+  br,bn = divmod(bd+360,360) # the bearing remainder and final bearing
+  
+  return bn
+
+def latLonDistance(lat1, lon1, lat2, lon2):
+  R = 6371 # Radius of the earth in km
+  dLat = deg2rad(lat2-lat1)  # deg2rad below
+  dLon = deg2rad(lon2-lon1) 
+  a = math.sin(dLat/2) * math.sin(dLat/2) + math.cos(deg2rad(lat1)) * math.cos(deg2rad(lat2)) * math.sin(dLon/2) * math.sin(dLon/2)
+  c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a)) 
+  d = R * c * 1000 #  Distance in m
+  return d
+
+
+def receiverDistance(lat, lon, altitude):
+  global args
+  distance = latLonDistance(lat, lon, args.lat, args.lon)
+  return float("{0:.3f}".format(distance))
+
+def copyMessage(data):
+    j = {}
+    j['loggedDate'] = data["loggedDate"]
+    j['time'] = calendar.timegm(datetime.datetime.utcnow().utctimetuple())
+    j['icao24'] = data["icao24"]
+    j['altitude'] = data["altitude"]
+    j['lat'] = data["lat"]
+    j['lon'] = data["lon"]
+    j['distance'] = float(data["distance"]/1000)
+    j['callsign'] = data["callsign"]
+
+    j['operator'] = data["operator"]
+    j['speed'] = data["groundSpeed"]
+    j['vspeed'] = data["verticalRate"]
+    j['heading'] = data["track"]
+    j['bearing'] = int(round(data["bearing"]))
+
+    # You never know what the data might contain :)
+    if "type" in data:
+      j['type'] = data["type"]
+    if "registration" in data:
+      j['registration'] = data["registration"]
+    if "image" in data:
+      j['image'] = data["image"]
+    if "copyright" in data:
+      j['copyright'] = data["copyright"]
+    if "origin" in data:
+      j["origin"] = data["origin"]
+    if "destination" in data:
+      j["destination"] = data["destination"]
+    return j
+
+def sendProx(mosq, data):
+    global args
+    j = copyMessage(data)
+    retain = False
+    mosq.publish(args.prox, json.dumps(j), 0, retain)
+
+def sendLost(mosq, data):
+    global args
+    j = copyMessage(data)
+    j["lost"] = True
+    retain = False
+    mosq.publish(args.prox, json.dumps(j), 0, retain)
+
+def proxyCheck(mosq, data):
+  global min_receiver_distance
+  global max_receiver_distance
+  global minTrackingDistance
+  global trackingICAO24
+  global gImageDB
+  global gCurImage
+
+  lat = data["lat"]
+  lon = data["lon"]
+  altitude = data["altitude"]
+  icao24 = data["icao24"]
+  lost = data["lost"]
+
+  if lat and lon:
+    distance = receiverDistance(lat, lon, altitude)
+    bearing = receiverBearing(lat, lon)
+    data["distance"] = distance
+    data["bearing"] = bearing
+
+    if distance < minTrackingDistance:
+      minTrackingDistance = distance
+      if icao24 != trackingICAO24:
+        if trackingICAO24:
+          log.debug("Giving up %s, tracking %s", trackingICAO24, icao24)
+        else:
+          log.debug("Tracking %s", icao24)
+        trackingICAO24 = icao24
+  # TODO: Lookup images here?
+        planeimage = gImageDB.find(trackingICAO24)
+        if planeimage:
+          gCurImage = planeimage.image
+        else:
+          searchTerm = "%s %s %s" % (data["operator"], data["type"], data["registration"])
+          (url, imageData) = google.imageSearch(searchTerm, 800)
+          if url and data:
+            gCurImage = url
+            log.debug("Added url %s for %s", gCurImage, icao24)
+            gImageDB.add(trackingICAO24, gCurImage)
+
+    data["image"] = gCurImage
+    if trackingICAO24 == icao24:
+      if lost:
+        log.debug("Lost%s", icao24)
+        trackingICAO24 = None
+        minTrackingDistance = 999999
+        sendLost(mosq, data)
+      else:
+        minTrackingDistance = distance
+        log.debug(" pos:%2f, %2f @ %dft distance:%d bearing:%d" % (lat, lon, altitude, distance, bearing))
+        sendProx(mosq, data)
+
+    if distance < min_receiver_distance:
+      log.info("New min range for %s at %d", icao24, distance)
+      min_receiver_distance = distance
+    if distance > max_receiver_distance:
+      log.info("New max range for %s at %d", icao24, distance)
+      max_receiver_distance = distance
+
+
+def mqttOnConnect(mosq, obj, rc):
+  global args
+  mosq.subscribe(args.radar, 0)
+  log.debug("MQTT Connect: %s" % (str(rc)))
+
+def mqttOnDisconnect(mosq, obj, rc):
+  global gQuitting
+  log.debug("MQTT Disconnect: %s" % (str(rc)))
+  if not gQuitting:
+    while not mqttConnect():
+      time.sleep(10)
+      log.debug("Attempting MQTT reconnect")
+    log.debug("MQTT connected")
+
+def mqttOnMessage(mosq, obj, msg):
+  try:
+    data = json.loads(msg.payload)
+  except Exception as e:
+    log.error("JSON load failed for '%s'", msg.payload)
+  proxyCheck(mosq, data)
+
+
+def mqttOnPublish(mosq, obj, mid):
+# log.debug("mid: "+str(mid)))
+    pass
+
+
+def mqttOnSubscribe(mosq, obj, mid, granted_qos):
+  log.debug("Subscribed")
+
+
+def mqttOnLog(mosq, obj, level, string):
+  log.debug("log:"+string)
+
+
+def mqttThread():
+  global gQuitting
+  global gImageDB
+  if args.imagedb:
+    gImageDB = planeimg.ImageDB(args.imagedb)
+  try:
+    mqttc.loop_forever()
+    gQuitting = True
+    log.debug("MQTT thread exiting")
+    gQuitting = True
+  except Exception as e:
+    log.error("MQTT thread got exception: %s" % (e))
+    print traceback.format_exc()
+    gQuitting = True
+    log.info("MQTT disconnect")
+    mqttc.disconnect();
+
+def mqttConnect():
+  global mqttc
+  global args
+  try:
+    mqttc = mosquitto.Mosquitto("proxclient-%d" % (random.randint(0, 65535)))
+    mqttc.on_message = mqttOnMessage
+    mqttc.on_connect = mqttOnConnect
+    mqttc.on_disconnect = mqttOnDisconnect
+    mqttc.on_publish = mqttOnPublish
+    mqttc.on_subscribe = mqttOnSubscribe
+    #mqttc.on_log = mqttOnLog # Uncomment to enable debug messages
+    mqttc.connect(args.mqtt_host, args.mqtt_port, 60)
+
+    thread = Thread(target = mqttThread)
+    thread.start()
+    return True
+  except socket.error, e:
+    return False
+
+  log.debug("MQTT wierdness")
+
+
+def loggingInit(level):
+  global args
+  log = logging.getLogger(__name__)
+
+  # Initialize remote logging
+  logger = logging.getLogger()
+  logger.setLevel(level)
+  if args.rlog_host:
+    remotelogger.init(logger = logger, appName = "radarprox", subSystem = None, host = args.rlog_host, level = level)
+
+  # Log to stdout
+  ch = logging.StreamHandler(sys.stdout)
+  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  ch.setFormatter(formatter)
+  logger.addHandler(ch)
+
+def signal_handler(signal, frame):
+  global gQuitting
+  global mqttc
+  print "ctrl-c"
+  gQuitting = True
+  mqttc.disconnect();
+  sys.exit(0)
+
+def main():
+  global gQuitting
+  global mqttc
+  global args
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-m', '--mqtt-host', help="MQTT broker hostname", default='127.0.0.1')
+  parser.add_argument('-p', '--mqtt-port', type=int, help="MQTT broker port number", default=1883)
+  parser.add_argument('-R', '--radar', help="MQTT radar topic to subscribe to", default='/adsb/radar/json')
+  parser.add_argument('-P', '--prox', help="MQTT proximity topic to publish to", default='/adsb/proximity/json')
+  parser.add_argument('-idb', '--imagedb', help="SQLite plane image database")
+  parser.add_argument('-r', '--rlog-host', help="Remote logging host")
+  parser.add_argument('-l', '--lat', type=float, help="Latitude of radar")
+  parser.add_argument('-L', '--lon', type=float, help="Longitude of radar")
+  parser.add_argument('-v', '--verbose', dest='verbose',  action="store_true", help="Verbose output")
+
+  args = parser.parse_args()
+
+  if not args.lat and not args.lon:
+    print "You really need to tell me where you are located (--lat and --lon)"
+    sys.exit(1)
+  if not args.imagedb:
+    print "You need to tell me where to store aircraft image info (--imagedb DB)"
+    sys.exit(1)
+  try:
+    signal.signal(signal.SIGINT, signal_handler)
+    if args.verbose:
+      loggingInit(logging.DEBUG)
+    else:
+      loggingInit(logging.INFO)
+    log.info("Proxclient started")
+    mqttConnect()
+    while not gQuitting:
+      time.sleep(1)
+  except Exception as e:
+    log.error("Mainloop got exception: %s" % (e))
+    print traceback.format_exc()
+    gQuitting = True
+  log.info("MQTT disconnect")
+  mqttc.disconnect();
+
+# Ye ol main
+main()
+
